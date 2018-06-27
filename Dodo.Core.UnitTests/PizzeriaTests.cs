2b7e33b205632bec55bfed36d5f296846f03ee75
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2018-06-27 22:27:23 +0300

    Test refactoring

--- Dodo.Core.UnitTests/PizzeriaTests.cs
+++ Dodo.Core.UnitTests/PizzeriaTests.cs
@@ -1,8 +1,5 @@
 using System;
-using Dodo.Core.Common;
-using Dodo.Core.DomainModel.Departments;
-using Dodo.Core.DomainModel.Departments.Units;
-using Dodo.Core.DomainModel.Management.Organizations;
+using Dodo.Core.UnitTests.DSL;
 using NUnit.Framework;
 
 namespace Dodo.Core.UnitTests
@@ -10,40 +7,22 @@ namespace Dodo.Core.UnitTests
     [TestFixture]
     public class PizzeriaTests
     {
-        private Pizzeria _pizzeria;
+        private ObjectMother _objectMother;
 
         [SetUp]
         public void Setup()
         {
-            _pizzeria = new Pizzeria(
-                id: 29,
-                uuid: new Uuid("000D3A240C719A8711E68ABA13F83227"),
-                name: "Сык-1",
-                alias: string.Empty,
-                translitAlias: string.Empty,
-                approve: UnitApprove.Approved,
-                state: UnitState.Open,
-                departmentId: 2,
-                departmentUuid: new Uuid("000D3A240C719A8711E68ABA13FC4A39"),
-                countryId: 1,
-                organization: null,
-                square: 100,
-                beginDateTimeWork: new DateTime(2010, 1, 2),
-                orientation: string.Empty,
-                cardPaymentPickup: null,
-                coordinateX: null,
-                coordinateY: null,
-                clientTreatment: ClientTreatment.Name,
-                terminalAtCourier: true,
-                pizzeriaFormat: null);
+           _objectMother = new ObjectMother();
         }
 
         [Test]
-        public void When_passed_DateTime_returns_correct_pizzeria_years_old()
+        public void When_pass_date_greater_than_pizzeria_begin_date_returns_correct_pizzeria_age_in_years()
         {
-            var date = new DateTime(2018, 1, 1);
+            var pizzeriaBeginWorkDate = new DateTime(2010, 1, 2);
+            var currentDate = new DateTime(2018, 1, 1);
+            var pizzeria = _objectMother.CreatePizzeriaWithBeginDateTimeWork(pizzeriaBeginWorkDate);
 
-            Assert.AreEqual(8, _pizzeria.GetYearsOld(date));
+            Assert.AreEqual(8, pizzeria.GetYearsOld(currentDate));
         }
     }
 }
