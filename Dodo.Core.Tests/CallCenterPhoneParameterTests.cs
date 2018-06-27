1184d9e0f5fedb2519151853c26a233d0887bc20
Mikhail Gasanov <mikhail.gasanov@corp.badoo.com>
2018-06-27 19:28:05 +0100

    First batch of tests added

---
+++ Dodo.Core.Tests/CallCenterPhoneParameterTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Dodo.Core.Common;
+using Dodo.Core.DomainModel.Departments;
+using Xunit;
+
+namespace Dodo.Core.Tests
+{
+    public class CallCenterPhoneParameterTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void NumberWithoutMarks__IfNumberIsNullOrEmpty_ThenReturnsNumberWithoutChanges(string value)
+        {
+            var callCenterPhoneParameter = new CallCenterPhoneParameter
+            {
+                Number = value,
+            };
+
+            var numberWithoutMarks = callCenterPhoneParameter.NumberWithoutMarks;
+            
+            Assert.Equal(numberWithoutMarks, value);
+        }
+        
+        [Theory]
+        [InlineData("321-67", "32167")]
+        [InlineData("(867) 53-09", "8675309")]
+        public void NumberWithoutMarks__IfNumberWithSplitters_ThenReturnsNumberWithoutSplitters(string value)
+        {
+            var number
+            var callCenterPhoneParameter = new CallCenterPhoneParameter
+            {
+                Number = value,
+            };
+
+            var numberWithoutMarks = callCenterPhoneParameter.NumberWithoutMarks;
+            
+            Assert.Equal(numberWithoutMarks, value);
+        }
+    }
+}
