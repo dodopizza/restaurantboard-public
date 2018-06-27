b392537d5bf2827c352a718635b3b3513e0a5ecd
Nikita Starichenko <n.starichenko@dodopizza.com>
2018-06-27 09:54:31 +0300

    Add project for tests

---
+++ Dodo.Core.Tests/DomainModel/OrganizationTests.cs
@@ -0,0 +1,18 @@
+﻿using Dodo.Core.Common;
+using Dodo.Core.DomainModel.Departments;
+using Dodo.Core.DomainModel.Finance;
+using Dodo.Core.DomainModel.Management.Organizations;
+using Xunit;
+
+namespace Dodo.Core.Tests.DomainModel
+{
+    public class OrganizationTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var country = new Country(0, "", "", 0, "", Currency.Ruble, "");
+            var organization = new Organization(0, new Uuid(), "", "", "", "", "", country, "", "", "");
+        }
+    }
+}
