5876f4815bc79c58e2848b1482792484db675076
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2018-06-28 15:57:18 +0300

    Add tests for CallCenterPhoneParameter

---
+++ Dodo.Core.UnitTests/DSL/CallCenterPhoneParameterBuilder.cs
@@ -0,0 +1,40 @@
+﻿using Dodo.Core.DomainModel.Departments;
+using Microsoft.VisualStudio.TestPlatform.Common.DataCollection;
+
+namespace Dodo.Core.UnitTests.DSL
+{
+    public class CallCenterPhoneParameterBuilder
+    {
+        private string _number;
+        private string _iconPath;
+        private string _iconSitePath;
+
+        public CallCenterPhoneParameterBuilder WithNumber(string number)
+        {
+            _number = number;
+            return this;
+        }
+
+        public CallCenterPhoneParameterBuilder WithIconPath(string iconPath)
+        {
+            _iconPath = iconPath;
+            return this;
+        }
+
+        public CallCenterPhoneParameterBuilder WithIconSitePath(string iconSitePath)
+        {
+            _iconSitePath = iconSitePath;
+            return this;
+        }
+
+        public CallCenterPhoneParameter Build()
+        {
+            return new CallCenterPhoneParameter
+            {
+                Number = _number,
+                IconPath = _iconPath,
+                IconSitePath = _iconSitePath,
+            };
+        }
+    }
+}
---
+++ Dodo.Core.UnitTests/DomainModel/Departments/CallCenterPhoneParameterTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using Dodo.Core.DomainModel.Departments;
+using Dodo.Core.UnitTests.DSL;
+
+namespace Dodo.Core.UnitTests.DomainModel.Departments
+{
+    [TestFixture]
+    public class CallCenterPhoneTests
+    {
+        private CallCenterPhoneParameterBuilder _builder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _builder = new CallCenterPhoneParameterBuilder();
+        }
+
+        [Test]
+        [TestCase("7(999)999-99-99")]
+        [TestCase("7(999)999-99 99")]
+        [TestCase("7 999 999-99-99")]
+        [TestCase("7  999  999 99 99")]
+        [TestCase("79999999999")]
+        public void When_pass_number_with_marks_returns_replaced_string_with_only_digits(string phoneNumber)
+        {
+            var phoneParameter = _builder
+                .WithNumber(phoneNumber)
+                .Build();
+
+            Assert.AreEqual("79999999999", phoneParameter.NumberWithoutMarks);
+        }
+
+        [Test]
+        public void When_pass_number_with_plus_sign_keep_it_in_result()
+        {
+            string phoneNumber = "+79999999999";
+
+            var phoneParameter = _builder
+                .WithNumber(phoneNumber)
+                .Build();
+
+            Assert.AreEqual("+79999999999", phoneParameter.NumberWithoutMarks);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+        public void When_pass_null_or_empty_returns_the_same_string(string phoneNumber)
+        {
+            var phoneParameter = _builder
+                .WithNumber(phoneNumber)
+                .Build();
+
+            Assert.AreEqual(phoneNumber, phoneParameter.NumberWithoutMarks);
+        }
+    }
+}
