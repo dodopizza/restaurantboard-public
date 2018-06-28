f05db00ed899a8fb70bd0a810e7ac326f696a82b
Andrew <Andrew25455@mail.ru>
2018-06-28 20:49:20 +0300

    Добавлен тест Uuid_Null для Dodo.Core.Common.Uuid

---
+++ Dodo.Core.Tests/UuidTest.cs
@@ -0,0 +1,20 @@
+﻿using Dodo.Core.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dodo.Core.Tests
+{
+    [TestClass]
+    public class UuidTest
+    {
+        [TestMethod]
+        public void Uuid_Null()
+        {
+            string input = null;
+            Assert.ThrowsException<ArgumentNullException>(() => { var sut = new Uuid(input); });
+        }
+        
+    }
+}
