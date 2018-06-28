cf2eba4359736123524cf1ec8a796b223993d52b
Andrew <Andrew25455@mail.ru>
2018-06-28 19:59:13 +0300

    Добавлен тест GetYearsOld_NotOpenedPizzeria для Dodo.Core.DomainModel.Departments.Units.Pizzeria

---
+++ Dodo.Core.Tests/PizzeriaTest.cs
@@ -0,0 +1,27 @@
+using Dodo.Core.DomainModel.Departments.Units;
+using Dodo.Core.DomainModel.Management.Organizations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Dodo.Core.Tests
+{
+    [TestClass]
+    public class PizzeriaTest
+    {
+        [TestMethod]
+        public void GetYearsOld_NotOpenedPizzeria()
+        {
+            var currentDate = DateTime.MinValue;
+            DateTime? beginDateTimeWork = null;
+            var sut = new Pizzeria(0, Common.Uuid.Empty, "", "", "", DomainModel.Departments.UnitApprove.Approved, DomainModel.Departments.UnitState.Close, 0, Common.Uuid.Empty, 0, mockOrganization, 0, beginDateTimeWork, "", null, null, null, ClientTreatment.DefaultName, false, mockPizzeriaFormat);
+            int expected = 0;
+
+            int result = sut.GetYearsOld(currentDate);
+
+            Assert.AreEqual(result, expected);
+        }
+
+        OrganizationShortInfo mockOrganization = new DomainModel.Management.Organizations.OrganizationShortInfo(0, "", "", null, "", "", "", 0, "", "", "");
+        PizzeriaFormat mockPizzeriaFormat = new PizzeriaFormat(0, "", "");
+    }
+}
