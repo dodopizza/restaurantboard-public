dfafeae46ad8fe289c70b47e3a01d8fcceb89c79
George Polevoy <george.polevoy@gmail.com>
2018-06-28 19:33:22 +0300

    add guid tests

---
+++ src/Dodo.Core.Tests/UuidTests.cs
@@ -0,0 +1,35 @@
+using System;
+using Dodo.Core.Common;
+using NUnit.Framework;
+
+namespace Dodo.Core.Tests
+{
+    public class UuidTests
+    {
+        [Test]
+        [TestCase("00000000000000000000000000000000")]
+        [TestCase("00000000000000000000000000000001")]
+        public void Constructor_AcceptsCorrectString(string s)
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.DoesNotThrow(() => new Uuid(s));
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void Constructor_ThrowsOnNullString(string s)
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.Throws<ArgumentNullException>(() => new Uuid(s));
+        }
+
+        [TestCase(1)]
+        [TestCase(31)]
+        public void Constructor_ThrowsWhenStringIsIncorrectLength(int len)
+        {
+            var s = new string('0', len);
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.Throws<ArgumentException>(() => new Uuid(s));
+        }
+    }
+}
