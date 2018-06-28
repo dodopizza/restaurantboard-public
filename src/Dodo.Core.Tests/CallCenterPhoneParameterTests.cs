ad9b114cd6ff8fe1cebc7663848b058a993d0d2f
George Polevoy <george.polevoy@gmail.com>
2018-06-28 19:55:03 +0300

    add cc phone number test
    Test cleaning of the phone number entered by user

---
+++ src/Dodo.Core.Tests/CallCenterPhoneParameterTests.cs
@@ -0,0 +1,22 @@
+﻿using Dodo.Core.DomainModel.Departments;
+using NUnit.Framework;
+
+namespace Dodo.Core.Tests
+{    
+    public class CallCenterPhoneParameterTests
+    {
+        [TestCase("+7(985)123-45-67", "+79851234567")]
+        [TestCase(" +7(985) 123-45-67", "+79851234567")]
+        public void NumberWithoutMarks_ReplacesCertainCharactersFromNumber(string dirtyPhoneNumber, string expected)
+        {
+            // Init
+            var phone = new CallCenterPhoneParameter {Number = dirtyPhoneNumber};
+
+            // Act
+            var actual = phone.NumberWithoutMarks;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
