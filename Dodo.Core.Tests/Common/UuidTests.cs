1111106462ff058acafa64b5866e4bdeac2d2a06
antonyzudov <antony.zudov@mail.ru>
2018-06-29 09:59:30 +0300

    Sests for Uuid

---
+++ Dodo.Core.Tests/Common/UuidTests.cs
@@ -0,0 +1,45 @@
+﻿using Dodo.Core.Common;
+using System;
+using Xunit;
+
+namespace Dodo.Core.Tests.Common
+{
+    public class UuidTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Uuid_ThrowArgumentNullException_IfEmptyOrNullString(string uuid)
+        {
+            Assert.Throws<ArgumentNullException>(() => new Uuid(uuid));
+        }
+
+        [Theory]
+        [InlineData("123")]
+        [InlineData("1234567890123456789012345678901234567890")]
+        public void Uuid_ThrowArgumentException_IfIncorrectLength(string uuid)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new Uuid(uuid));
+            Assert.Equal("The length of the String for UUID must be exactly 32 chars.\r\nParameter name: uuid", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("12345678901234z67890123456789012")]
+        public void Uuid_ThrowArgumentException_IfNotGuid(string uuid)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new Uuid(uuid));
+            Assert.Equal("UUId must have the same characters like guid", exception.Message);
+        }
+
+        [Fact]
+        public void Uuid_ShouldCreate_IfGuid()
+        {
+            var guid = Guid.NewGuid().ToString().Replace("-", "");
+
+            var uuid = new Uuid(guid);
+
+            Assert.Equal(uuid.ToString(), guid);
+        }
+
+    }
+}
