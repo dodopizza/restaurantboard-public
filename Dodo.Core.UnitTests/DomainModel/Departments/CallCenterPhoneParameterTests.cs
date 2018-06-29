7aa0c99d6d0a52764abe0b94ed124ad9e6d3b0f7
y.khodartsevich <y.khodartsevich@dodopizza.com>
2018-06-29 10:05:17 +0300

    add 2 tests for GetIconUrl

--- Dodo.Core.UnitTests/DomainModel/Departments/CallCenterPhoneParameterTests.cs
+++ Dodo.Core.UnitTests/DomainModel/Departments/CallCenterPhoneParameterTests.cs
@@ -52,5 +52,47 @@ namespace Dodo.Core.UnitTests.DomainModel.Departments
 
             Assert.AreEqual(phoneNumber, phoneParameter.NumberWithoutMarks);
         }
+        
+        
+        [Test]
+        public void When_pass_icon_site_path_returns_the_same_string()
+        {
+            string iconSitePathParameter = "http://we/are/the/best/team";
+            string host = "localhost:5000";
+
+            var iconSitePath = _builder
+                .WithIconSitePath(iconSitePathParameter)
+                .Build();
+
+            Assert.AreEqual("http://we/are/the/best/team", iconSitePath.GetIconUrl(host));
+        }
+        
+        [Test]
+        [TestCase("we/are/the/best/team\\")]
+        [TestCase("we/are\\the/best/team\\")]
+        public void When_pass_icon_path_returns_string_with_updated_host_and_end(string iconPathParameter)
+        {
+            string host = "localhost:5000\\";
+
+            var iconPath = _builder
+                .WithIconPath(iconPathParameter)
+                .Build();
+
+            Assert.AreEqual("localhost:5000/we/are/the/best/team/", iconPath.GetIconUrl(host));
+        }
+        
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        public void When_pass_empty_icon_path_returns_empty_string(string iconPathParameter)
+        {
+            string host = "localhost:5000\\";
+
+            var iconPath = _builder
+                .WithIconPath(iconPathParameter)
+                .Build();
+
+            Assert.AreEqual("", iconPath.GetIconUrl(host));
+        }
     }
 }
