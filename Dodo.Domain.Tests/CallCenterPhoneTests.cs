4406c61f47db02babf521bf378a4a80ac621a952
AlexanderIB31 <aleks_bales@mail.ru>
2018-06-30 21:39:26 +0300

    Added serialization/deserialization tests

--- Dodo.Domain.Tests/CallCenterPhoneTests.cs
+++ Dodo.Domain.Tests/CallCenterPhoneTests.cs
@@ -1,4 +1,6 @@
-﻿using Dodo.Core.DomainModel.Departments;
+﻿using System.Xml;
+using System.Xml.Linq;
+using Dodo.Core.DomainModel.Departments;
 using NUnit.Framework;
 
 namespace Dodo.Domain.Tests
@@ -23,7 +25,7 @@ namespace Dodo.Domain.Tests
 
 		[Test]
 		[Sequential]
-		public void GetIconUrl_WithIconPath_ShouldGenerateUrl(
+		public void GetIconUrl_WithIconPath_UrlContainsIconPath(
 			[Values("test", "test\\test2")] string iconPath,
 			[Values(Host + "/test", Host + "/test/test2")] string expectedIconUrl)
 		{
@@ -38,17 +40,70 @@ namespace Dodo.Domain.Tests
 		}
 
 		[Test]
-		public void GetIconUrl_WithIconSitePath_ShouldGenerateUrl()
+		public void GetIconUrl_WithIconSitePath_UrlContainsIconSitePath()
 		{
 			const string expected = "www.sitepath.ru/icon";
 			var phone = new CallCenterPhoneParameter
 			{
-				IconSitePath = "www.sitepath.ru/icon"
+				IconSitePath = expected
 			};
 
 			var actualIconUrl = phone.GetIconUrl(Host);
 
 			Assert.AreEqual(expected, actualIconUrl);
+		}
+
+		[Test]
+		[Combinatorial]
+		public void SerializeObject_ShouldReturnedXmlElement(
+			[Values("123", null)]string number,
+			[Values("www.example.com", null)]string iconSitePath,
+			[Values("www.tedt.com/test", null)]string iconPath)
+		{
+			var obj = new CallCenterPhoneParameter()
+			{
+				Number = number,
+				IconPath = iconPath,
+				IconSitePath = iconSitePath
+			};
+
+			var result = obj.CreateXmlNode().ToString();
+			var expected = $"<Phone number=\"{number}\" iconPath=\"{iconPath}\" iconSitePath=\"{iconSitePath}\" />";
+			
+			Assert.AreEqual(expected, result);
+		}
+
+		[Test]
+		public void DeserializeXml_IfEmptyContainer_ShouldReturnEmptyArray()
+		{
+			var container = new XElement("Phone");
+			
+			var result = CallCenterPhoneParameter.GetCallCenterPhonesFromXml(container);
+			
+			Assert.IsEmpty(result);
+		}
+		
+		[Test]
+		public void DeserializeXml_ShouldReturnObject(
+			[Values("123", "")]string number,
+			[Values("www.example.com", "")]string iconSitePath,
+			[Values("www.tedt.com/test", "")]string iconPath)
+		{
+			var expected = new CallCenterPhoneParameter()
+			{
+				Number = number,
+				IconPath = iconPath,
+				IconSitePath = iconSitePath
+			};
+			var container = new XElement("Root",
+				new XElement("CallCenterPhones", expected.CreateXmlNode()));
+			
+			var result = CallCenterPhoneParameter.GetCallCenterPhonesFromXml(container);
+			
+			Assert.AreEqual(1, result.Length);
+			Assert.AreEqual(expected.Number, result[0].Number);
+			Assert.AreEqual(expected.IconPath, result[0].IconPath);
+			Assert.AreEqual(expected.IconSitePath, result[0].IconSitePath);
 		}
 	}
 }
