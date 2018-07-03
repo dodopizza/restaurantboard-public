0a2bdc60eb52089ae3bb6b285757b2ed90eb3dcd
Administrator <Administrator@DESKTOP-0ION1EL>
2018-07-03 22:08:55 +0300

    refactor UuidTests and DepartmentStructureTests

--- Dodo.RestaurantBoard.Tests/DepartmentsStructureServiceTests.cs
+++ Dodo.RestaurantBoard.Tests/DepartmentsStructureServiceTests.cs
@@ -12,6 +12,7 @@ namespace Dodo.RestaurantBoard.Tests
     {
 
         IDepartmentsStructureService departmentsStructureService;
+        int sampleDepartmentId = 1;
 
         public DepartmentsStructureServiceTests()
         {
@@ -20,32 +21,33 @@ namespace Dodo.RestaurantBoard.Tests
         }
 
         [Fact]
-        public void PizzaIsNotNull()
+        public void DepartmentsStructureServiceTests_GetPizzeria_Pizzeria_PizzaIsNotNull()
         {
             var service = departmentsStructureService;
-            var pizza = service.GetPizzeriaOrCache(1);
-            Assert.NotNull(pizza);
-        }
-        [Theory]
-        [InlineData(1)]
-        [InlineData(2)]
-        [InlineData(3)]
 
-        public void IsDepartmentCountryAlwaysRussia(int departmentId)
-        {
-            var department = departmentsStructureService.GetDepartmentOrCache<Department>(departmentId);
-            Assert.Equal("Russia", department.Country.Name);
+            var pizzeria = service.GetPizzeriaOrCache(1);
+
+            Assert.NotNull(pizzeria);
         }
 
-        [Theory]
-        [InlineData(1)]
-        [InlineData(2)]
-        [InlineData(3)]
-
-        public void IsDepartmentByUidCurrencyAlwaysRubble(int iud)
+        [Fact]
+        public void DepartmentsStructureServiceTests_GetDepartmentCountryName_Department_CountryAlwaysRussia()
         {
-            var department = departmentsStructureService.GetDepartmentByUnitOrCache(iud);
-            Assert.Equal(Currency.Ruble, department.Country.Currency);
+            var department = departmentsStructureService.GetDepartmentOrCache<Department>(sampleDepartmentId);
+
+            var departmentCountryName = department.Country.Name;
+
+            Assert.Equal("Russia", departmentCountryName);
+        }
+
+        [Fact]
+        public void DepartmentsStructureServiceTests_GetDepartmentCountryCurrency_Department_CountryCurrencyAlwaysRubble()
+        {
+            var department = departmentsStructureService.GetDepartmentByUnitOrCache(sampleDepartmentId);
+
+            var contryCurrency = department.Country.Currency;
+
+            Assert.Equal(Currency.Ruble, contryCurrency);
         }
 
 
---
+++ Dodo.RestaurantBoard.Tests/UuidTests.cs
@@ -0,0 +1,50 @@
+﻿using Dodo.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Dodo.RestaurantBoard.Tests
+{
+    public class UuidTests
+    {
+        [Fact]
+        public void UuidTests_CreateUuidWithEmptyString_Uuid_ThrowArgumentNullExceptin()
+        {
+            //Arrange
+            Action createEmptyUUidAction = () =>  new Uuid("");
+            
+            //Act Assert
+            var exc = Assert.Throws<ArgumentNullException>(()=> createEmptyUUidAction());
+        }
+
+        [Fact]
+        public void UuidTests_CreateUuidWithNotEqual32LengthString_UUid_ThrowArgumentExeption()
+        {
+            Action createEmptyUUidAction = () => new Uuid(new String('0',31));
+
+            var exc = Assert.Throws<ArgumentException>(() => createEmptyUUidAction());
+        }
+
+        [Fact]
+        public void UuidTests_CreateUuidWithUnsuitableCharacters_Uuid_ThrowArgumentExeptionWithSpecificMessage()
+        {
+            Action createEmptyUUidAction = () => new Uuid($"r{new String('0', 31)}");
+
+
+            var exc = Assert.Throws<ArgumentException>(() => createEmptyUUidAction());
+            Assert.Equal("UUId must have the same characters like guid", exc.Message);
+        }
+
+        [Fact]
+        public void UuidTests_UseToStringMethod_Uuid_ToStringMethodIsOverrided()
+        {
+            var uuidStr = new String('0', 32);
+            var uuid = new Uuid(uuidStr);
+
+            var toStringResult = uuid.ToString();
+
+            Assert.Equal(uuidStr, toStringResult);
+        }
+    }
+}
