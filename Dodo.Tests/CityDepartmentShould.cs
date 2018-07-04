0c7af89c493c2d1b576ec344841300429ca3cf3c
chernecova <chernecova@bars-open.ru>
2018-07-04 21:33:35 +0300

    Переделаны тесты для CityDepartment

---
+++ Dodo.Tests/CityDepartmentShould.cs
@@ -0,0 +1,35 @@
+﻿using Dodo.Core.DomainModel.Departments;
+using Dodo.Core.DomainModel.Departments.Departments;
+using Xunit;
+
+namespace Dodo.Tests
+{
+    public class CityDepartmentShould
+    {
+        [Fact]
+        public void SetZeroTimeShiftForMoscowTimezone()
+        {
+            Department testDepartment = new CityDepartment
+            {
+                TimeZoneUTCOffset = 180
+            };
+
+            var actualTimeZoneShift = testDepartment.TimeZoneShift;
+
+            Assert.Equal(0, actualTimeZoneShift);
+        }
+
+        [Fact]
+        public void SetMinus3HoursTimeShiftForZeroUtcTimezone()
+        {
+            Department testDepartment = new CityDepartment
+            {
+                TimeZoneUTCOffset = 0
+            };
+
+            var actualTimeZoneShift = testDepartment.TimeZoneShift;
+
+            Assert.Equal(-3, actualTimeZoneShift);
+        }
+    }
+}
