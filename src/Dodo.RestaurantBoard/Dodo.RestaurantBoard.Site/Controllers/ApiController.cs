bcca3fbc773ffd765e41bd6369b98769281a4908
Andrew <Andrew25455@mail.ru>
2018-07-11 22:16:35 +0300

    Добавили контроллер API. Добавили метод получения пиццерии по Id.

---
+++ src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Controllers/ApiController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dodo.Core.DomainModel.Departments.Units;
+using Dodo.Core.Services;
+using Dodo.RestaurantBoard.Domain.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Dodo.RestaurantBoard.Site.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApiController : Controller
+    {
+        private readonly IDepartmentsStructureService _departmentsStructureService;
+        private readonly IClientsService _clientsService;
+        private readonly IManagementService _managementService;
+        private readonly ITrackerClient _trackerClient;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public ApiController(
+            IDepartmentsStructureService departmentsStructureService,
+            IClientsService clientsService,
+            IManagementService managementService,
+            ITrackerClient trackerClient,
+            IHostingEnvironment hostingEnvironment
+            )
+        {
+            _departmentsStructureService = departmentsStructureService;
+            _clientsService = clientsService;
+            _managementService = managementService;
+            _trackerClient = trackerClient;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet("/pizzeria/{id}")]
+        public JsonResult GetPizzeria(int id)
+        {
+            return Json(_departmentsStructureService.GetPizzeriaOrCache(id));
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
