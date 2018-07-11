3cc19fecc1f3d08d250332507c8d5840991e13ac
Mikhail Gasanov <mikhail.gasanov@corp.badoo.com>
2018-07-11 21:01:21 +0100

    First statecheck test

--- Dodo.Tests/TrackerClientShould.cs
+++ Dodo.Tests/TrackerClientShould.cs
@@ -1,5 +1,10 @@
-﻿using Dodo.RestaurantBoard.Domain.Services;
+﻿using Dodo.Core.Common;
+using Dodo.RestaurantBoard.Domain.Services;
+using Dodo.Tracker.Contracts;
+using Dodo.Tracker.Contracts.Enums;
+using Moq;
 using Xunit;
+
 
 namespace Dodo.Tests
 {
@@ -8,7 +13,34 @@ namespace Dodo.Tests
         [Fact]
         public void ReturnAllOrders_IfIsExpiringParameterNotSpecifiedAndDefaultValueIsUsed()
         {
-            var trackerClient = new TrackerClient();
+            var expectedOrders = new ProductionOrder[]
+            {
+                new ProductionOrder
+                {
+                    Id = 1,
+                    Number = 3,
+                    ClientName = "Misha"
+                },
+                new ProductionOrder
+                {
+                    Id = 2,
+                    Number = 4,
+                    ClientName = "Tanya"
+                },
+            };
+            var ordersProviderStub = new Mock<IOrdersProvider>();
+            ordersProviderStub.Setup(p => p.GetOrders()).Returns(expectedOrders);
+            var trackerClient = new TrackerClient(ordersProviderStub.Object);
+
+            var actualOrders = trackerClient.GetOrders(new Uuid(), OrderType.Delivery, new OrderState[1], 0);
+            
+            Assert.Equal(expectedOrders, actualOrders);
+        }
+
+        [Fact]
+        public void ReturnOnlyExpiringOrders_IfIsExpiringParameterIsEqualToTrue()
+        {
+            
         }
     }
 }
