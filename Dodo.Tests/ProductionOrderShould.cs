6e40eae9f15934319fdfce8bd1cf3713cf5df20c
Mikhail Gasanov <mikhail.gasanov@corp.badoo.com>
2018-07-11 22:32:24 +0100

    ProductionOrder tests templates

---
+++ Dodo.Tests/ProductionOrderShould.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Dodo.Tracker.Contracts;
+using Xunit;
+
+namespace Dodo.Tests
+{
+    public class ProductionOrderShould
+    {
+        [Fact]
+        public void ReturnTrue_WhenIsExpiringIsCalled_AndNowParameterIsGreaterThanChangeDateAtLeastForOneHour()
+        {
+            var order = new ProductionOrder()
+            {
+                ChangeDate = DateTime.Parse("07/11/2018 23:00")
+            };
+
+            var isExpiring = order.IsExpiring(DateTime.Parse("07/11/2018 21:59"));
+            
+            Assert.True(isExpiring);
+        }
+        
+        [Fact]
+        public void ReturnFalse_WhenIsExpiringIsCalled_AndNowParameterIsLessThanChangeDate()
+        {
+            
+        }
+        
+        [Fact]
+        public void ReturnFalse_WhenIsExpiringIsCalled_AndNowParameterIsEqualToChangeDate()
+        {
+            
+        }
+    }
+}
