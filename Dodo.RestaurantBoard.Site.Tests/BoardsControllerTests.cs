39035ddb853a1b7acd925024dbbc31f0c911cb8f
Timur Basyrov <timmthetiger@mail.ru>
2018-07-13 18:54:10 +0500

    Add state test for OrdersReadinessToStationary action

--- Dodo.RestaurantBoard.Site.Tests/BoardsControllerTests.cs
+++ Dodo.RestaurantBoard.Site.Tests/BoardsControllerTests.cs
@@ -1,10 +1,16 @@
 using System;
+using Dodo.Core.DomainModel.Departments;
+using Dodo.Core.DomainModel.Departments.Departments;
+using Dodo.Core.DomainModel.Finance;
 using Moq;
 using NUnit.Framework;
 using Dodo.Core.Services;
 using Dodo.RestaurantBoard.Domain.Services;
 using Dodo.RestaurantBoard.Site.Controllers;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using AutoFixture;
+using Dodo.Core.DomainModel.Departments.Units;
 
 namespace Dodo.RestaurantBoard.Site.Tests
 {
@@ -16,17 +22,39 @@ namespace Dodo.RestaurantBoard.Site.Tests
             var departmentsStructureServiceStub = new Mock<IDepartmentsStructureService>();
             departmentsStructureServiceStub.Setup(x => x.GetDepartmentByUnitOrCache(It.IsAny<int>()))
                 .Returns(() => null);
+            var boardsController = CreateController(departmentsStructureServiceStub.Object);
+
+            Assert.Throws<ArgumentException>(() => boardsController.OrdersReadinessToStationary(42));
+        }
+
+        [Test]
+        public void OrdersReadinessToStationary_ReturnsViewResult_WhenDepartmentIsNotNull()
+        {
+            var fixture = new Fixture();
+            var departmentsStructureServiceStub = new Mock<IDepartmentsStructureService>();
+            departmentsStructureServiceStub.Setup(x => x.GetDepartmentByUnitOrCache(It.IsAny<int>()))
+                .Returns(fixture.Create<CityDepartment>());
+            departmentsStructureServiceStub.Setup(x => x.GetPizzeriaOrCache(It.IsAny<int>()))
+                .Returns(fixture.Create<Pizzeria>());
+            var boardsController = CreateController(departmentsStructureServiceStub.Object);
+
+            var result = boardsController.OrdersReadinessToStationary(42);
+
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+
+        private BoardsController CreateController(IDepartmentsStructureService departmentsStructureService)
+        {
             var dummyClientsService = new Mock<IClientsService>();
             var dummyManagementService = new Mock<IManagementService>();
             var dummyTrackerClient = new Mock<ITrackerClient>();
             var dummyHostingEnvironment = new Mock<IHostingEnvironment>();
-            var boardsController = new BoardsController(departmentsStructureServiceStub.Object,
+
+            return new BoardsController(departmentsStructureService,
                 dummyClientsService.Object,
                 dummyManagementService.Object,
                 dummyTrackerClient.Object,
-                dummyHostingEnvironment.Object);            
-
-            Assert.Throws<ArgumentException>(() => boardsController.OrdersReadinessToStationary(42));
+                dummyHostingEnvironment.Object);
         }
     }
 }
