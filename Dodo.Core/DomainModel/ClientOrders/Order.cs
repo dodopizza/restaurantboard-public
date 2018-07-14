7b28b942fcb0abbfadefffcff88d194ea39de9b6
Artem Trofimushkin <artemtrofimushkin@gmail.com>
2018-07-14 13:16:37 +0300

    Introduce models and interface for behavior tests

---
+++ Dodo.Core/DomainModel/ClientOrders/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace Dodo.Core.DomainModel.ClientOrders
+{
+	public class Order
+	{
+		public int OrderId { get; set; }
+		public int OrderNumber { get; set; }
+		public string ClientName { get; set; }
+		public string ClientIconPath { get; set; }
+		public long OrderReadyTimestamp { get; set; }
+		public string OrderReadyDateTime { get; set; }
+	}
+}
---
+++ Dodo.Core/Services/IOrdersService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Dodo.Core.DomainModel.ClientOrders;
+
+namespace Dodo.Core.Services
+{
+	public interface IOrdersService
+	{
+		GetOrdersResult GetOrdersForUnit(int unitId, IDictionary<string, object> viewData, ICollection<int> currentProductsIds);
+	}
+}
