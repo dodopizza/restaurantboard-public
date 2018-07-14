a3c47c0b836e1a993804b6402fa72f3c7707251b
Xneg <xnegxneg@gmail.com>
2018-07-14 14:15:30 +0300

    Исправление замечаний Сергея.

--- Dodo.Tests/TrackerClientTests.cs
+++ Dodo.Tests/TrackerClientTests.cs
@@ -4,8 +4,6 @@ using Dodo.Tracker.Contracts;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Dodo.Tests
@@ -19,7 +17,6 @@ namespace Dodo.Tests
 
             trackerClient.AddProductionOrder("John", 3);
             trackerClient.AddProductionOrder("John", 2);
-            
 
             Assert.Equal(3 + 2, trackerClient.GetOrderByName("John").Number);
         }
@@ -32,11 +29,22 @@ namespace Dodo.Tests
             trackerClient.AddProductionOrder("John", 3);
             trackerClient.AddProductionOrder("Tom", 2);
 
-            var johnResult = trackerClient.GetOrderByName("John");
             var tomResult = trackerClient.GetOrderByName("Tom");
 
-            Assert.Equal(3, johnResult.Number);
             Assert.Equal(2, tomResult.Number);
+        }
+
+        [Fact]
+        public void NotChangingOldOrder_WhenAddingWithDifferentClientNames()
+        {
+            var trackerClient = new TrackerClient(GetOrderStorageTest());
+
+            trackerClient.AddProductionOrder("John", 3);
+            trackerClient.AddProductionOrder("Tom", 2);
+
+            var johnResult = trackerClient.GetOrderByName("John");
+
+            Assert.Equal(3, johnResult.Number);
         }
 
         [Fact]
@@ -72,25 +80,43 @@ namespace Dodo.Tests
 
             var orderUpdateDate = trackerClient.GetOrderByName("John").ChangeDate;
 
-            Assert.True(orderDateAdd != orderUpdateDate);
+            Assert.True(orderDateAdd.Value.AddSeconds(10) == orderUpdateDate.Value);
         }
 
         [Fact]
-        public void OnAddProductionOrderWithExitingOrder_CallUpdateProductionOrder()
+        public void ReturnsExpectedOrders_WhenGetOrdersAfterDate()
+        {
+            var orderStorageStub = new Mock<IOrdersStorage>();
+
+            var expectedOrders = new[] { new ProductionOrder { ChangeDate = new DateTime(2018, 12, 01) } };
+            var allOrders = new List<ProductionOrder>(expectedOrders);
+            allOrders.Add(new ProductionOrder { ChangeDate = new DateTime(2018, 01, 01) });
+
+            orderStorageStub.Setup(o => o.GetAllProductionOrders()).Returns(allOrders);
+
+            var trackerClient = new TrackerClient(orderStorageStub.Object);
+
+            var orders = trackerClient.GetOrdersAfterDate(new DateTime(2018, 06, 01));
+
+            Assert.Equal(expectedOrders, orders);
+        }
+
+        [Fact]
+        public void OnAddProductionOrderWithExistingOrder_CallUpdateProductionOrder()
         {
             var orderStorageMock = new Mock<IOrdersStorage>();
             orderStorageMock.Setup(o => o.GetProductionOrderByName(It.IsAny<string>()))
-                .Returns(new ProductionOrder() {Id= 5 });            
+                .Returns(new ProductionOrder() { Id = 5, Number = 3 });            
 
             var trackerClient = new TrackerClient(orderStorageMock.Object);
 
             trackerClient.AddProductionOrder("John", 1);
 
-            orderStorageMock.Verify(o => o.UpdateProductionOrder(5, null, 1), Times.Once);
+            orderStorageMock.Verify(o => o.UpdateProductionOrder(5, null, 1 + 3), Times.Once);
 
         }
         [Fact]
-        public void OnAddProductionOrderWithoutExitingOrder_CallAddProductionOrder()
+        public void OnAddProductionOrderWithoutExistingOrder_CallAddProductionOrder()
         {
             var orderStorageMock = new Mock<IOrdersStorage>();
             orderStorageMock.Setup(o => o.GetProductionOrderByName(It.IsAny<string>())).Returns<ProductionOrder>(null);
@@ -100,24 +126,6 @@ namespace Dodo.Tests
             trackerClient.AddProductionOrder("John", 1);
 
             orderStorageMock.Verify(o => o.AddProductionOrder("John", 1), Times.Once);
-        }
-
-        [Fact]
-        public void ReturnsOrderWithDateAfter_WhenGetOrdersAfterDate_()
-        {
-            var orderStorageMock = new Mock<IOrdersStorage>();
-
-            var expectedOrders = new[] { new ProductionOrder { ChangeDate = new DateTime(2018, 12, 01) } };
-            var allOrders = new List<ProductionOrder>(expectedOrders);
-            allOrders.Add(new ProductionOrder { ChangeDate = new DateTime(2018, 01, 01) });
-
-            orderStorageMock.Setup(o => o.GetAllProductionOrders()).Returns(allOrders);
-
-            var trackerClient = new TrackerClient(orderStorageMock.Object);
-
-            var orders = trackerClient.GetOrdersAfterDate(new DateTime(2018, 06, 01));
-
-            Assert.Equal(expectedOrders, orders);
         }
 
         private IOrdersStorage GetOrderStorageTest(IDateTimeProvider dateTimeProvider = null)
