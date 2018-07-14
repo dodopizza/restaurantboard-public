c9c6a54c0d5a3f8c6e2e717981490a8418a1356f
Artem Trofimushkin <artemtrofimushkin@gmail.com>
2018-07-14 13:17:11 +0300

    Interface implementation

---
+++ src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Domain/DodoFMProxy.cs
@@ -0,0 +1,12 @@
+﻿namespace Dodo.RestaurantBoard.Domain
+{
+	public class DodoFMProxy
+	{
+		private const string DODO_FM_KEY = "DodoFMSong";
+
+		public static string GetSongName()
+		{
+			return "I will always love you";
+		}
+	}
+}
---
+++ src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Domain/Services/IconPathService.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Dodo.Core.DomainModel.Clients;
+using Dodo.Core.DomainModel.Departments.Units;
+using Dodo.Core.DomainModel.OrderProcessing;
+using Dodo.Core.Services;
+
+namespace Dodo.RestaurantBoard.Domain.Services
+{
+	public class IconPathService : IIconPathService
+	{
+	    public string GetIconPath(RestaurantReadnessOrders orders, ClientTreatment clientTreatment, ClientIcon[] icons)
+	    {
+		    if (!(clientTreatment == ClientTreatment.RandomImage && icons.Any()))
+		    {
+			    return null;
+		    }
+
+		    const string host = "https://wedevstorage.blob.core.windows.net/";
+		    var iconIndex = orders.OrderNumber % icons.Length;
+		    return icons[iconIndex].GetUrl(host);
+		}
+	}
+}
