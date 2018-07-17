08be7c8bfa4425eb7fe2db698c52b39e2a84b3c0
DetyshevSA <ruinrut@gmail.com>
2018-07-17 22:04:03 +0300

    Второй тест

--- Dodo.RestarauntBoardTests/OrdersStoreTests.cs
+++ Dodo.RestarauntBoardTests/OrdersStoreTests.cs
@@ -28,12 +28,10 @@ namespace Dodo.RestarauntBoardTests
         [Fact]
         public void ShoudContainExpiredOrder_WhenGetExpiredOrders()
         {
-            var orderDate = new DateTime(2018, 1, 1);
-            var order = new ProductionOrder(){OrderDate =  orderDate };
-            var orderStore = new OrdersStore();
-            orderStore.AddOrder(order);
+            var order = Create.Order.WithDate("01-01-2018").Please();
+            var orderStore = Create.OrderStore.With(order).Please();
 
-            var expiredOrders = orderStore.GetExpiredOrders(orderDate.AddSeconds(order.ExpirationTime+1));
+            var expiredOrders = orderStore.GetExpiredOrders(order.ExpireDate());
 
             Assert.Contains(order, expiredOrders);
         }
@@ -42,13 +40,10 @@ namespace Dodo.RestarauntBoardTests
         [Fact]
         public void ShoudNotContainUnExpiredOrder_WhenGetExpiredOrders()
         {
-           
-            var orderDate = new DateTime(2018, 1, 1);
-            var order = new ProductionOrder(){OrderDate =  orderDate };
-            var orderStore = new OrdersStore();
-            orderStore.AddOrder(order);
+            var order = Create.Order.WithDate("01-01-2018").Please();
+            var orderStore = Create.OrderStore.With(order).Please();
 
-            var expiredOrders = orderStore.GetExpiredOrders(orderDate);
+            var expiredOrders = orderStore.GetExpiredOrders(order.Date());
 
             Assert.Empty(expiredOrders);
         }
@@ -98,5 +93,73 @@ namespace Dodo.RestarauntBoardTests
 
             productOrderMock.Verify(p => p.IsExpired(It.IsAny<DateTime>()), Times.Never);
         }
+
     }
+
+    internal static class Create
+    {
+        public static OrdersStoreBuilder OrderStore => new OrdersStoreBuilder();
+        public static ProductionOrderBuilder Order => new ProductionOrderBuilder();
+
+    }
+
+    public class ProductionOrderBuilder
+    {
+        private ProductionOrder _order;
+
+        public ProductionOrderBuilder()
+        {
+            _order = new ProductionOrder();
+        }
+
+        internal ProductionOrderBuilder WithDate(string dateString)
+        {
+            var date = DateTime.Parse(dateString);
+            _order.OrderDate = date;
+
+            return this;
+        }
+
+        internal ProductionOrder Please()
+        {
+            return _order;
+        }
+    }
+
+    public class OrdersStoreBuilder
+    {
+        private OrdersStore _ordersStore;
+        private ProductionOrder _order;
+
+        public OrdersStoreBuilder()
+        {
+            _ordersStore = new OrdersStore();
+            _order = new ProductionOrder();
+        }
+
+        internal OrdersStore Please()
+        {
+            return _ordersStore;
+        }
+
+        internal OrdersStoreBuilder With(ProductionOrder order)
+        {
+            _order = order;
+            return this;
+        }
+    }
+
+    public static class ProductioOrderExtensions
+    {
+        public static DateTime ExpireDate(this ProductionOrder order)
+        {
+            return order.OrderDate.AddMilliseconds(order.ExpirationTime + 1);
+        }
+
+        public static DateTime Date(this ProductionOrder order)
+        {
+            return order.OrderDate;
+        }
+    }
+
 }
