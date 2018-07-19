6765b88f4837173e1e9006cd1ddbf44d28e083d2
Yana Khodartsevich <y.khodartsevich@dodopizza.com>
2018-07-19 19:59:36 +0300

    add IsOneYearOld_WhenOpenedYearFromViewDate()

---
+++ Dodo.Core.Tests/DomainModel/Departments/Units/PizzeriaTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Dodo.Core.DomainModel.Departments;
+using Dodo.Core.DomainModel.Departments.Units;
+using Xunit;
+
+namespace Dodo.Core.Tests.DomainModel.Departments
+{
+    public class PizzeriaTest
+    {
+        [Fact]
+        public void IsZeroYearsOld_WhenPizzeriaIsNotOpened()
+        {
+            Pizzeria pizzeria = CreatePizzeria()
+                .ThatIsNotOpened()
+                .Please();
+            
+            PizzeriaAssert.That(pizzeria).At(DateTime.Now).HasAgeOf(0);
+        }
+
+        [Fact]
+        public void IsOneYearOld_WhenOpenedYearFromViewDate()
+        {
+            Pizzeria pizzeria = CreatePizzeria()
+                .ThatIsOpened(14.JulyOf(2019))
+                .Please();
+            
+            PizzeriaAssert.That(pizzeria).At(14.JulyOf(2020)).HasAgeOf(1);
+        }
+
+        private PizzeriaBuilder CreatePizzeria()
+        {
+            return new PizzeriaBuilder();
+        }
+    }
+
+    public static class IntegerExtensions
+    {
+        public static DateTime JulyOf(this int day, int year)
+        {
+            return new DateTime(year, 7, day);
+        }
+    }
+
+    public class PizzeriaAssert
+    {
+        private Pizzeria _pizzeria;
+        private DateTime? currentDateTime;
+
+        private PizzeriaAssert(Pizzeria pizzeria)
+        {
+            _pizzeria = pizzeria;
+        }
+
+        public static PizzeriaAssert That(Pizzeria pizzeria)
+        {
+            return new PizzeriaAssert(pizzeria);
+        }
+
+        public void HasAgeOf(int age)
+        {
+            if (currentDateTime == null)
+            {
+                throw new Exception("Setup incomplete: set not-null currentDateTime");
+            }
+            Assert.Equal(age, _pizzeria.GetYearsOld(currentDateTime.Value));
+        }
+
+        public PizzeriaAssert At(DateTime date)
+        {
+            currentDateTime = date;
+            return this;
+        }
+    }
+
+    internal class PizzeriaBuilder
+    {
+        private DateTime? openingDate;
+
+        public PizzeriaBuilder ThatIsNotOpened()
+        {
+            openingDate = null;
+            return this;
+        }
+
+        public PizzeriaBuilder ThatIsOpened(DateTime date)
+        {
+            openingDate = date;
+            return this;
+        }
+
+        public Pizzeria Please()
+        {
+            return new Pizzeria(0, null, "", null, "", UnitApprove.Approved, UnitState.Close, 0, null, 0, null, 0.0,
+                openingDate,
+                "", null, null, null, ClientTreatment.DefaultName, false, null);
+        }
+    }
+}
