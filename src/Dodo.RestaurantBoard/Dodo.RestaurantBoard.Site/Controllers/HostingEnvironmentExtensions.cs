c8e08fcdbeda7bdebf04e99109c8fc6b84fc9184
antonyzudov <antony.zudov@mail.ru>
2018-08-07 19:44:27 +0300

    перенос LocalozedContext в HostingEnvironmentExtensions

--- src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Controllers/HostingEnvironmentExtensions.cs
+++ src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Controllers/HostingEnvironmentExtensions.cs
@@ -8,23 +8,34 @@ using Microsoft.AspNetCore.Hosting;
 namespace Dodo.RestaurantBoard.Site.Controllers {
     public static class LocalizedContext
     {
-        private const string _localizedResourcesFolder = "LocalizedResources";
-
         public static string LocalizedContent(IHostingEnvironment hostingEnvironment, string contentPath)
         {
             if (string.IsNullOrEmpty(contentPath))
-                throw new ArgumentNullException(nameof (contentPath));
-            StringBuilder stringBuilder = new StringBuilder();
-            CultureInfo currentUiCulture = Thread.CurrentThread.CurrentUICulture;
-            int serverPathLength = hostingEnvironment.WebRootPath.Length - 1;
+            {
+                throw new ArgumentNullException(nameof(contentPath));
+            }
+                
+            var stringBuilder = new StringBuilder();
+            var currentUiCulture = Thread.CurrentThread.CurrentUICulture;
+
+            var serverPathLength = hostingEnvironment.WebRootPath.Length - 1;
+
             contentPath = contentPath.Trim('/');
-            string path1 = hostingEnvironment.WebRootPath + "/" + Path.Combine("LocalizedResources", currentUiCulture.TwoLetterISOLanguageName, contentPath);
+            var path1 = hostingEnvironment.WebRootPath + "/"
+                        + Path.Combine("LocalizedResources", currentUiCulture.TwoLetterISOLanguageName, contentPath);
+
             if (File.Exists(path1))
+            {
                 return ConvertLocalPathToRelativeUrl(path1, serverPathLength);
+            }
+
             stringBuilder.AppendLine(path1);
-            string path2 = hostingEnvironment.WebRootPath + "/" + Path.Combine("LocalizedResources", contentPath);
+            var path2 = hostingEnvironment.WebRootPath + "/" + Path.Combine("LocalizedResources", contentPath);
             if (File.Exists(path2))
+            {
                 return ConvertLocalPathToRelativeUrl(path2, serverPathLength);
+            }
+
             stringBuilder.AppendLine(path2);
             throw new FileNotFoundException(stringBuilder.ToString());
         }
