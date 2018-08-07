19bb54cca015d54b466d0ddac1484ba04f06ceb1
chernecova <chernecova@bars-open.ru>
2018-08-07 20:59:09 +0300

    CallCenterPhoneParameter.NumberWithoutMarks refactoring

--- Dodo.Core/DomainModel/Departments/CallCenterPhoneParameter.cs
+++ Dodo.Core/DomainModel/Departments/CallCenterPhoneParameter.cs
@@ -1,76 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 
 namespace Dodo.Core.DomainModel.Departments
 {
-	[Serializable]
-	public class CallCenterPhoneParameter
-	{
-		public String Number { get; set; }
+    [Serializable]
+    public class CallCenterPhoneParameter
+    {
+        public String Number { get; set; }
 
-		/// <summary>
-		/// Путь на внешнем сервере (файл сервер)
-		/// </summary>
-		public String IconPath { set; get; }
+        /// <summary>
+        /// Путь на внешнем сервере (файл сервер)
+        /// </summary>
+        public String IconPath { set; get; }
 
-		/// <summary>
-		/// Путь в текущем каталоге сайта
-		/// </summary>
-		public String IconSitePath { set; get; }
+        /// <summary>
+        /// Путь в текущем каталоге сайта
+        /// </summary>
+        public String IconSitePath { set; get; }
 
+        public String NumberWithoutMarks
+        {
+            get
+            {
+                var exceptChars = new[] { '-', '(', ')', ' ' };
+                return new string(Number?.Where(x => !exceptChars.Contains(x)).ToArray());
+            }
+        }
 
-		public String GetIconUrl(String host)
-		{
-			if (!String.IsNullOrEmpty(IconPath))
-			{
-				return (host.TrimEnd('/', '\\') + "/" + IconPath).Replace('\\', '/');
-			}
+        public String GetIconUrl(String host)
+        {
+            if (!String.IsNullOrEmpty(IconPath))
+            {
+                return (host.TrimEnd('/', '\\') + "/" + IconPath).Replace('\\', '/');
+            }
 
-			if (!String.IsNullOrEmpty(IconSitePath))
-			{
-				return IconSitePath;
-			}
+            if (!String.IsNullOrEmpty(IconSitePath))
+            {
+                return IconSitePath;
+            }
 
-			return String.Empty;
-		}
-
-
-		public String NumberWithoutMarks
-		{
-			get
-			{
-				if (String.IsNullOrEmpty(Number))
-					return Number;
-
-				String[] replacedMarks = { "-", " ", "(", ")" };
-				String replacedNumber = Number;
-				foreach (String mark in replacedMarks)
-				{
-					replacedNumber = replacedNumber.Replace(mark, "");
-				}
-				return replacedNumber;
-			}
-		}
+            return String.Empty;
+        }
 
 
 
-		public XElement CreateXmlNode()
-		{
-			return new XElement("Phone",
-				new XAttribute("number", Number ?? String.Empty),
-				new XAttribute("iconPath", IconPath ?? String.Empty),
-				new XAttribute("iconSitePath", IconSitePath ?? String.Empty));
-		}
+        public XElement CreateXmlNode()
+        {
+            return new XElement("Phone",
+                new XAttribute("number", Number ?? String.Empty),
+                new XAttribute("iconPath", IconPath ?? String.Empty),
+                new XAttribute("iconSitePath", IconSitePath ?? String.Empty));
+        }
 
-		public static CallCenterPhoneParameter[] GetCallCenterPhonesFromXml(XElement container)
-		{
+        public static CallCenterPhoneParameter[] GetCallCenterPhonesFromXml(XElement container)
+        {
             return container.Element("CallCenterPhones")?
                 .Elements()
                 .Select(CreateCallCenterParameter)
                 .ToArray();
-		}
+        }
 
         private static CallCenterPhoneParameter CreateCallCenterParameter(XElement element)
         {
