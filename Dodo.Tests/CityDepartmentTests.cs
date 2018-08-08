92f21887d7521ba84993bb355e6e02fd330cbc2d
Xneg <xnegxneg@gmail.com>
2018-08-08 20:35:09 +0300

    Тест на свойство Department.TimeZoneUTCOffsetString.

---
+++ Dodo.Tests/CityDepartmentTests.cs
@@ -0,0 +1,17 @@
+using Dodo.Core.DomainModel.Departments.Departments;
+using System;
+using Xunit;
+
+namespace Dodo.Tests
+{
+    public class CityDepartmentTests
+    {
+        [Fact]
+        public void ShouldReturnZeroUtcOffset()
+        {
+            var cityDepartment = new CityDepartment();
+
+            Assert.Equal("-00:00", cityDepartment.TimeZoneUTCOffsetString);
+        }
+    }
+}
