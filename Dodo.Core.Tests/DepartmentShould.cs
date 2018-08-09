0104791160391975b71ffbf3297699138a9c4133
AlexanderIB31 <aleks_bales@mail.ru>
2018-08-09 22:28:51 +0300

    Move getting math symbol to GetTimeZoneShiftSymbol() and write test for Department property TimeZoneShiftString

---
+++ Dodo.Core.Tests/DepartmentShould.cs
@@ -0,0 +1,20 @@
+﻿using Dodo.Core.DomainModel.Departments;
+using Moq;
+using Xunit;
+
+namespace Dodo.Core.Tests
+{
+    public class DepartmentShould
+    {
+        [Fact]
+        public void ReturnPlus2String_WhenTimeZoneShiftIs2()
+        {
+            var departmentStub = new Mock<Department> { CallBase = true };
+            departmentStub.SetupGet(x => x.TimeZoneShift).Returns(2);
+
+            var timeZoneShiftString = departmentStub.Object.TimeZoneShiftString;
+
+            Assert.Equal("+2", timeZoneShiftString);
+        }
+    }
+}
--- Dodo.Core/DomainModel/Departments/Department.cs
+++ Dodo.Core/DomainModel/Departments/Department.cs
@@ -7,216 +7,210 @@ using Dodo.Core.Common;
 
 namespace Dodo.Core.DomainModel.Departments
 {
-	[Serializable]
-	public abstract class Department : Entity
-	{
-		public virtual Uuid Uuid { get; set; }
-		public virtual String Name { get; set; }
+    [Serializable]
+    public abstract class Department : Entity
+    {
+        public virtual Uuid Uuid { get; set; }
+        public virtual String Name { get; set; }
 
-		public virtual DepartmentType Type { get; set; }
+        public virtual DepartmentType Type { get; set; }
 
-		public virtual DepartmentState State { get; set; }
+        public virtual DepartmentState State { get; set; }
 
-		/// <summary>
-		/// Отклонение от UTC в минутах
-		/// </summary>
-		public virtual Int32 TimeZoneUTCOffset { get; set; }
+        /// <summary>
+        /// Отклонение от UTC в минутах
+        /// </summary>
+        public virtual Int32 TimeZoneUTCOffset { get; set; }
 
-		/// <summary>
-		/// Отклонение от серверного времени(Москва) в часах
-		/// </summary>
-		public virtual Int16 TimeZoneShift
-		{
-			get
-			{
-				Double currentTimeZoneUTCOffset = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow).TotalMinutes;
-				return (Int16)Math.Round(((Double)TimeZoneUTCOffset - currentTimeZoneUTCOffset) / 60);
-			}
-		}
+        /// <summary>
+        /// Отклонение от серверного времени(Москва) в часах
+        /// </summary>
+        public virtual Int16 TimeZoneShift
+        {
+            get
+            {
+                Double currentTimeZoneUTCOffset = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow).TotalMinutes;
+                return (Int16)Math.Round(((Double)TimeZoneUTCOffset - currentTimeZoneUTCOffset) / 60);
+            }
+        }
 
-		public virtual Country Country { get; set; }
+        public virtual Country Country { get; set; }
 
-		public virtual String OwnerName  { get; set; }
-		public virtual String OwnerPhone  { get; set; }
-		public virtual String OwnerEMail  { get; set; }
+        public virtual String OwnerName  { get; set; }
+        public virtual String OwnerPhone  { get; set; }
+        public virtual String OwnerEMail  { get; set; }
 
-		public virtual String TimeZoneUTCOffsetString
-		{
-			get
-			{
-				Char mathSimbol;
-				if (TimeZoneUTCOffset > 0)
-					mathSimbol = '+';
-				else if (TimeZoneShift < 0)
-					mathSimbol = '-';
-				else
-					mathSimbol = ' ';
+        public virtual String TimeZoneUTCOffsetString
+        {
+            get
+            {
+                Char mathSimbol;
+                if (TimeZoneUTCOffset > 0)
+                    mathSimbol = '+';
+                else if (TimeZoneShift < 0)
+                    mathSimbol = '-';
+                else
+                    mathSimbol = ' ';
 
-				TimeSpan timeSpan = TimeSpan.FromMinutes(Math.Abs(TimeZoneUTCOffset));
-				String fromTimeString = timeSpan.ToString(@"hh\:mm");
+                TimeSpan timeSpan = TimeSpan.FromMinutes(Math.Abs(TimeZoneUTCOffset));
+                String fromTimeString = timeSpan.ToString(@"hh\:mm");
 
-				return String.Format("{0}{1}", mathSimbol, fromTimeString);
-			}
-		}
+                return String.Format("{0}{1}", mathSimbol, fromTimeString);
+            }
+        }
 
-		public virtual String TimeZoneShiftString
-		{
-			get
-			{
-				Char mathSimbol;
-				if (TimeZoneShift > 0)
-					mathSimbol = '+';
-				else if (TimeZoneShift < 0)
-					mathSimbol = '-';
-				else
-					mathSimbol = ' ';
+        public virtual String TimeZoneShiftString =>
+            String.Format("{0}{1}", GetTimeZoneShiftSymbol(), Math.Abs(TimeZoneShift));
 
-				return String.Format("{0}{1}", mathSimbol, Math.Abs(TimeZoneShift));
-			}
-		}
+        private Char GetTimeZoneShiftSymbol()
+        {
+            if (TimeZoneShift > 0) return '+';
+            if (TimeZoneShift < 0) return '-';
+            return  ' ';
+        }
 
-		public virtual TimeSpan CurrentTimeZoneUTCOffset => TimeSpan.FromMinutes(TimeZoneUTCOffset);
+        public virtual TimeSpan CurrentTimeZoneUTCOffset => TimeSpan.FromMinutes(TimeZoneUTCOffset);
 
-		/// <summary>
-		/// Приводит dateTime ко времени в UTC с учетом часового пояса департамента
-		/// </summary>
-		/// <param name="dateTime"></param>
-		/// <returns></returns>
-		public DateTime GetUtcDateTime(DateTime dateTime)
-		{
-			return DateTime.SpecifyKind(dateTime.AddMinutes(-TimeZoneUTCOffset), DateTimeKind.Utc);
-		}
+        /// <summary>
+        /// Приводит dateTime ко времени в UTC с учетом часового пояса департамента
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public DateTime GetUtcDateTime(DateTime dateTime)
+        {
+            return DateTime.SpecifyKind(dateTime.AddMinutes(-TimeZoneUTCOffset), DateTimeKind.Utc);
+        }
 
-		public DateTime? ToLocalDateTime(DateTime? utcDateTime)
-		{
-			return utcDateTime == null
-				? (DateTime?)null
-				: DateTime.SpecifyKind(utcDateTime.Value.AddMinutes(TimeZoneUTCOffset), DateTimeKind.Unspecified);
-		}
+        public DateTime? ToLocalDateTime(DateTime? utcDateTime)
+        {
+            return utcDateTime == null
+                ? (DateTime?)null
+                : DateTime.SpecifyKind(utcDateTime.Value.AddMinutes(TimeZoneUTCOffset), DateTimeKind.Unspecified);
+        }
 
-		/// <summary>
-		/// Текущее время подразделения (с учетом часового пояса)
-		/// </summary>
-		public virtual DateTime CurrentDateTime => DateTime.SpecifyKind( DateTime.UtcNow.AddMinutes(TimeZoneUTCOffset), DateTimeKind.Unspecified);
+        /// <summary>
+        /// Текущее время подразделения (с учетом часового пояса)
+        /// </summary>
+        public virtual DateTime CurrentDateTime => DateTime.SpecifyKind( DateTime.UtcNow.AddMinutes(TimeZoneUTCOffset), DateTimeKind.Unspecified);
 
-		public virtual DateTime CurrentDateTimeUtc => GetUtcDateTime(CurrentDateTime);
+        public virtual DateTime CurrentDateTimeUtc => GetUtcDateTime(CurrentDateTime);
 
-		public virtual DateTime CurrentDate => CurrentDateTime.Date;
+        public virtual DateTime CurrentDate => CurrentDateTime.Date;
 
 		
 		
-		protected Department(Int32 id, Uuid uuid, String name, DepartmentType type,  DepartmentState state, Int32 timeZoneUTCOffset,  Country country)
-		{
-			Id = id;
-			Uuid = uuid;
-			Name = name;
-			Type = type;
-			State = state;
-			Country = country;
+        protected Department(Int32 id, Uuid uuid, String name, DepartmentType type,  DepartmentState state, Int32 timeZoneUTCOffset,  Country country)
+        {
+            Id = id;
+            Uuid = uuid;
+            Name = name;
+            Type = type;
+            State = state;
+            Country = country;
 
-			TimeZoneUTCOffset = timeZoneUTCOffset;
-		}
+            TimeZoneUTCOffset = timeZoneUTCOffset;
+        }
 
-		protected Department(Int32 id, Uuid uuid, String name, DepartmentType type,  DepartmentState state,
-			Int32 timeZoneUTCOffset,  Country country, String ownerName, String ownerPhone, String ownerEMail)
-		{
-			Id = id;
-			Uuid = uuid;
-			Name = name;
-			Type = type;
-			State = state;
-			Country = country;
-			OwnerName = ownerName;
-			OwnerPhone = ownerPhone;
-			OwnerEMail = ownerEMail;
+        protected Department(Int32 id, Uuid uuid, String name, DepartmentType type,  DepartmentState state,
+            Int32 timeZoneUTCOffset,  Country country, String ownerName, String ownerPhone, String ownerEMail)
+        {
+            Id = id;
+            Uuid = uuid;
+            Name = name;
+            Type = type;
+            State = state;
+            Country = country;
+            OwnerName = ownerName;
+            OwnerPhone = ownerPhone;
+            OwnerEMail = ownerEMail;
 
-			TimeZoneUTCOffset = timeZoneUTCOffset;
-		}
+            TimeZoneUTCOffset = timeZoneUTCOffset;
+        }
 
-		public Department ( )
-		{
-		}
+        public Department ( )
+        {
+        }
 
-		public static DepartmentParameters GetDepartmentParametersFromXmlString(String value, DepartmentType departmentType)
-		{
-			if (String.IsNullOrEmpty(value)) return null;
+        public static DepartmentParameters GetDepartmentParametersFromXmlString(String value, DepartmentType departmentType)
+        {
+            if (String.IsNullOrEmpty(value)) return null;
 
-			switch (departmentType)
-			{
-				case DepartmentType.Department:
-					return CityParameters.FromXmlString(value);
+            switch (departmentType)
+            {
+                case DepartmentType.Department:
+                    return CityParameters.FromXmlString(value);
 
-				default:
-					return null;
-			}
-		}
+                default:
+                    return null;
+            }
+        }
 
-		public override String ToString()
-		{
-			return $"{Name} Type: {Type} State: {State}";
-		}
+        public override String ToString()
+        {
+            return $"{Name} Type: {Type} State: {State}";
+        }
 
-		#region мультиязычность
+        #region мультиязычность
 
-		private Cultures[] availableCultures;
+        private Cultures[] availableCultures;
 
-		public Cultures[] AvailableCultures
-		{
-			get { return availableCultures; }
-			set
-			{
-				availableCultures = value;
-				departmentCultureData = GetCultureData(CurrentCultureName, DepartmentCultureData, availableCultures);
-			}
-		}
+        public Cultures[] AvailableCultures
+        {
+            get { return availableCultures; }
+            set
+            {
+                availableCultures = value;
+                departmentCultureData = GetCultureData(CurrentCultureName, DepartmentCultureData, availableCultures);
+            }
+        }
 
-		public String CurrentCultureName { get; set; }
+        public String CurrentCultureName { get; set; }
 
-		private String nativeCultureName;
+        private String nativeCultureName;
 
-		public String NativeCultureName
-		{
-			get
-			{
-				if (AvailableCultures != null && AvailableCultures.Any(c => (c.IsNative)))
-					return AvailableCultures.FirstOrDefault(c => c.IsNative).CultureName;
-				else
-					return nativeCultureName;
-			}
-			set { nativeCultureName = value; }
-		}
+        public String NativeCultureName
+        {
+            get
+            {
+                if (AvailableCultures != null && AvailableCultures.Any(c => (c.IsNative)))
+                    return AvailableCultures.FirstOrDefault(c => c.IsNative).CultureName;
+                else
+                    return nativeCultureName;
+            }
+            set { nativeCultureName = value; }
+        }
 
-		private DepartmentCultureData[] departmentCultureData;
+        private DepartmentCultureData[] departmentCultureData;
 
-		public DepartmentCultureData[] DepartmentCultureData
-		{
-			get { return departmentCultureData; }
-			set
-			{
-				departmentCultureData = GetCultureData(CurrentCultureName, value, AvailableCultures);
-			}
-		}
+        public DepartmentCultureData[] DepartmentCultureData
+        {
+            get { return departmentCultureData; }
+            set
+            {
+                departmentCultureData = GetCultureData(CurrentCultureName, value, AvailableCultures);
+            }
+        }
 
-		private DepartmentCultureData[] GetCultureData(String selectedCultureName, DepartmentCultureData[] cultureData, Cultures[] availableCultures)
-		{
-			List<DepartmentCultureData> result = new List<DepartmentCultureData>();
+        private DepartmentCultureData[] GetCultureData(String selectedCultureName, DepartmentCultureData[] cultureData, Cultures[] availableCultures)
+        {
+            List<DepartmentCultureData> result = new List<DepartmentCultureData>();
 
-			if (availableCultures == null || cultureData == null)
-				return cultureData;
+            if (availableCultures == null || cultureData == null)
+                return cultureData;
 
-			foreach (var culture in availableCultures)
-			{
-				if (cultureData.Any(c => (c.CultureName == culture.CultureName)))
-					result.Add(cultureData.FirstOrDefault(c => (c.CultureName == culture.CultureName)));
-				else if (String.Equals(NativeCultureName, culture.CultureName))
-					result.Add(new DepartmentCultureData(0, culture.CultureName, this.Id, this.Name));
-				else
-					result.Add(new DepartmentCultureData(0, culture.CultureName, this.Id, ""));
-			}
+            foreach (var culture in availableCultures)
+            {
+                if (cultureData.Any(c => (c.CultureName == culture.CultureName)))
+                    result.Add(cultureData.FirstOrDefault(c => (c.CultureName == culture.CultureName)));
+                else if (String.Equals(NativeCultureName, culture.CultureName))
+                    result.Add(new DepartmentCultureData(0, culture.CultureName, this.Id, this.Name));
+                else
+                    result.Add(new DepartmentCultureData(0, culture.CultureName, this.Id, ""));
+            }
 
-			return result.ToArray();
-		}
+            return result.ToArray();
+        }
 
-		#endregion		
-	}
+        #endregion		
+    }
 }
