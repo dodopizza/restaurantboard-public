8c57aadd83ed328083e94f3b065ba35f54d94ab5
AlexanderIB31 <aleks_bales@mail.ru>
2018-08-09 21:52:01 +0300

    temp commit

--- src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Controllers/BoardsController.cs
+++ src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Controllers/BoardsController.cs
@@ -102,13 +102,6 @@ namespace Dodo.RestaurantBoard.Site.Controllers
                 .Select(MapToRestaurantReadnessOrders)
                 .ToArray();
 
-            var clientTreatment = pizzeria.ClientTreatment;
-            ClientIcon[] icons = { };
-            if (clientTreatment == ClientTreatment.RandomImage)
-            {
-                icons = _clientsService.GetIcons();
-            }
-
             var playTineParamIds = orders.Select(x => x.OrderId).ToArray();
             ViewData["PlayTune"] = playTineParamIds.Except(CurrentProductsIds).Any() ? 1 : 0;
             CurrentProductsIds = playTineParamIds;
@@ -124,9 +117,7 @@ namespace Dodo.RestaurantBoard.Site.Controllers
                             x.OrderId,
                             x.OrderNumber,
                             x.ClientName,
-                            ClientIconPath = clientTreatment == ClientTreatment.RandomImage && icons.Any()
-                                ? GetIconPath(x.OrderNumber, icons, "https://wedevstorage.blob.core.windows.net/")
-                                : null,
+                            ClientIconPath = GetClientIconPath(pizzeria.ClientTreatment, x.OrderNumber),
                             OrderReadyTimestamp = x.OrderReadyDateTime.Ticks,
                             OrderReadyDateTime = x.OrderReadyDateTime.ToString(CultureInfo.CurrentUICulture)
                         })
@@ -134,6 +125,17 @@ namespace Dodo.RestaurantBoard.Site.Controllers
             };
 
             return Json(result);
+        }
+
+        private string GetClientIconPath(ClientTreatment clientTreatment, int orderNumber)
+        {
+            if (clientTreatment == ClientTreatment.RandomImage 
+                && _clientsService.GetIcons().Any())
+            {
+                return GetIconPath(orderNumber, _clientsService.GetIcons(), "https://wedevstorage.blob.core.windows.net/");
+            }
+
+            return null;
         }
 
         private static RestaurantReadnessOrders MapToRestaurantReadnessOrders(ProductionOrder order)
