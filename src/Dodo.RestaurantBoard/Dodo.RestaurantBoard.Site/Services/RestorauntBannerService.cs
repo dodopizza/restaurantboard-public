e3b347c959b2620f9161166cf37a885275ea074f
Artem Trofimushkin <a.trofimushkin@dodopizza.com>
2018-08-14 19:59:08 +0300

    Sprout class refactoring step 2

--- src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Services/RestorauntBannerService.cs
+++ src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Services/RestorauntBannerService.cs
@@ -4,16 +4,49 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dodo.Core.DomainModel.Departments.Departments;
+using Dodo.Core.DomainModel.Management;
+using Dodo.Core.Services;
 
 namespace Dodo.RestaurantBoard.Site.Services
 {
     public class RestorauntBannerService
     {
-        IHostingEnvironment _hostingEnvironment;
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IDepartmentsStructureService _departmentsStructureService;
+        private readonly IManagementService _managementService;
 
-        public RestorauntBannerService(IHostingEnvironment hostingEnvironment)
+        public RestorauntBannerService(
+            IHostingEnvironment hostingEnvironment, 
+            IDepartmentsStructureService departmentsStructureService, 
+            IManagementService managementService)
         {
             _hostingEnvironment = hostingEnvironment;
+            _departmentsStructureService = departmentsStructureService;
+            _managementService = managementService;
+        }
+        
+        public IEnumerable<object> GetBanners(int countryId, int departmentId, int unitId)
+        {
+            var restaurantBanners = GetAvailableBanners(countryId, departmentId, unitId);
+
+            IEnumerable<object> result;
+
+            if (restaurantBanners.Any())
+            {
+                result = restaurantBanners.Select(
+                    x => new
+                    {
+                        BannerUrl = x.Url.Replace('\\', '/'),
+                        DisplayTime = x.DisplayTime * 1000
+                    });
+            }
+            else
+            {
+                result = GetDefaultResult();
+            }
+
+            return result;
         }
 
         public IEnumerable<object> GetDefaultResult()
@@ -31,6 +64,15 @@ namespace Dodo.RestaurantBoard.Site.Services
         protected virtual string GetDefaultBannerUrl()
         {
             return LocalizedContext.LocalizedContent(_hostingEnvironment, "Tracking-Scoreboard-Empty.jpg");
+        }
+
+        protected virtual IEnumerable<RestaurantBanner> GetAvailableBanners(int countryId, int departmentId, int unitId)
+        {
+            var department = _departmentsStructureService.GetDepartmentOrCache<CityDepartment>(departmentId);
+            var restaurantBanners = _managementService
+                .GetAvailableBanners(countryId, unitId, department.CurrentDateTime)
+                .Where(x => x.MenuSpecializationTypes.Any(q => q == department.MenuSpecializationType));
+            return restaurantBanners;
         }
     }
 }
