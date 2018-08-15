c4877157ad13ed7be43b95dd9262728da64a6e49
Surgerer <aleksey.gorin@gmail.com>
2018-08-15 20:02:42 +0300

    Add test VersionHelperShould

---
+++ Dodo.Tests/VersionHelperShould.cs
@@ -0,0 +1,23 @@
+﻿using Dodo.RestaurantBoard.Site.Common.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace Dodo.Tests
+{
+    public class VersionHelperShould
+    {
+        [Fact]
+        public void ReturnUrlWithVersionToken_OnRequest()
+        {
+            var url = "http://localhost";
+            var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString(2);
+
+            var urlwithToken = VersionHelper.AddVersionToken(url);
+
+            Assert.Equal($"{url}?v={assemblyVersion}", urlwithToken);
+        }
+    }
+}
