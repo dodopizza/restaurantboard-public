d35e7c61d8d8684b4d94be04bd240c34a447affd
Surgerer <aleksey.gorin@gmail.com>
2018-08-15 21:46:09 +0300

    Paralel design Time Zone New

---
+++ Dodo.Tests/TimeZoneNewShould.cs
@@ -0,0 +1,21 @@
+﻿using Dodo.Core.New.DomainModel.Departments;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Dodo.Tests
+{
+    public class TimeZoneNewShould
+    {
+        [Fact]
+        public void NegativeTimeZoneMainOfficeOffsetString_WhenMainOfficeTimeZoneUTCOffsetNegative()
+        {
+            var timeZone = new TimeZoneNew(-3);
+
+            var offsetString = timeZone.TimeZoneMainOfficeOffsetString();
+
+            Assert.Contains("-", offsetString);
+        }
+    }
+}
