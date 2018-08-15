7ff623a2e69b63eb874ab5d10c806a94a8c86b57
Ivan G <ivan.s.grishulenko@gmail.com>
2018-08-15 20:25:53 +0700

    Add test

---
+++ Dodo.RestaurantBoard.Site.Tests/VersionHelperTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Dodo.RestaurantBoard.Site.Common.Helpers;
+using NUnit.Framework;
+
+namespace Dodo.RestaurantBoard.Site.Tests
+{
+    public class VersionHelperTest
+    {
+        [Test]
+        public void AddVersionToken_AddsQuestionMark()
+        {
+            // Arrange
+            var url = "foo";
+            
+            // Act
+            var urlWithVersion = VersionHelper.AddVersionToken(url);
+
+            // Assert
+            Assert.True(urlWithVersion.Contains("?"));
+        }
+    }
+}
