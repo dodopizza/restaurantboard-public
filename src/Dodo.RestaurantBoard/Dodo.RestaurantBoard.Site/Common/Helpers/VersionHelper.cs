7dd9b6fa6e24c58814b741c57ed365cca149e8da
Ivan G <ivan.s.grishulenko@gmail.com>
2018-08-15 20:41:02 +0700

    static to instance, step 3

--- src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Common/Helpers/VersionHelper.cs
+++ src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Common/Helpers/VersionHelper.cs
@@ -10,6 +10,9 @@ namespace Dodo.RestaurantBoard.Site.Common.Helpers
 		public const string VERSION_QUERY_PARAMETER = "v";
 
 	    public static string GetVersionToken() =>
+	        VERSION_QUERY_PARAMETER + "=" + Assembly.GetExecutingAssembly().GetName().Version.ToString(2);
+
+        public string GetVersionTokenNew() =>
 			VERSION_QUERY_PARAMETER + "=" + Assembly.GetExecutingAssembly().GetName().Version.ToString(2);
 
 		public static string AddVersionToken(string url)
