7ee3c67914e8de94e798fae4b831dd4860308d01
Xneg <xnegxneg@gmail.com>
2018-08-17 22:17:55 +0300

    * Сделал класс VersionHelper нестатическим. * Сделал метод GetVersionToken нестатическим.

--- src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Common/Helpers/VersionHelper.cs
+++ src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Site/Common/Helpers/VersionHelper.cs
@@ -5,19 +5,19 @@ using System.Reflection;
 namespace Dodo.RestaurantBoard.Site.Common.Helpers
 {
 	[Description("VersionHelper у каждой сборки должен быть свой, т.к. он зависит от Assembly")]
-	public static class VersionHelper
+	public class VersionHelper
 	{
 		public const string VERSION_QUERY_PARAMETER = "v";
 
 		private static readonly Version _version =
 			Assembly.GetExecutingAssembly().GetName().Version;
 
-		public static string GetVersionToken() =>
+		public string GetVersionToken() =>
 			VERSION_QUERY_PARAMETER + "=" + _version.ToString(2);
 
 		public static string AddVersionToken(string url)
 		{
-			var versionToken = GetVersionToken();
+			var versionToken = new VersionHelper().GetVersionToken();
 			var queryStart = url.IndexOf("?", StringComparison.Ordinal);
 			url = queryStart == -1
 				? url + "?" + versionToken
