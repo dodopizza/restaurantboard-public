729425bba10035be548b370966cfa9321a48bb82
Administrator <Administrator@DESKTOP-0ION1EL>
2018-08-18 19:24:07 +0300

    Create TestableTrackerClient

--- Dodo.Tests/TrackerClientShould.cs
+++ Dodo.Tests/TrackerClientShould.cs
@@ -3,6 +3,7 @@ using Dodo.RestaurantBoard.Domain.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Dodo.Tracker.Contracts;
 using Xunit;
 
 namespace Dodo.Tests
@@ -12,11 +13,36 @@ namespace Dodo.Tests
         [Fact]
         public void ReturnSingleProductOrder_WhenLimitOne()
         {
-            var trackerClient = new TrackerClient();
+            var trackerClient = new TestableTrackerClient();
 
             var orders = trackerClient.GetOrdersByCount(new Uuid(), Tracker.Contracts.Enums.OrderType.Delivery,null,1);
 
             Assert.Single(orders);
+        }
+
+
+        public class TestableTrackerClient:TrackerClient
+        {
+            public override ProductionOrder[] QueryOrders()
+            {
+                var orders = new[]
+                {
+                    new ProductionOrder
+                    {
+                        Id = 55,
+                        Number = 3,
+                        ClientName = "Пупа"
+                    },
+                    new ProductionOrder
+                    {
+                        Id = 56,
+                        Number = 4,
+                        ClientName = "Лупа"
+                    },
+                };
+
+                return orders;
+            }
         }
     }
 }
