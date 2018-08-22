56b80eb92bb247c880f5fdb062faea5afdaa2bb0
AlexanderIB31 <aleks_bales@mail.ru>
2018-08-23 00:18:25 +0300

    Test GetOrderReadinessToStationary_ShouldReturnOrderForPupaClientName complete

--- Dodo.RestaurantBoard.Tests/BoardsControllerTest.cs
+++ Dodo.RestaurantBoard.Tests/BoardsControllerTest.cs
@@ -1,5 +1,10 @@
-﻿using Dodo.RestaurantBoard.Site.Controllers;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dodo.RestaurantBoard.Site.Controllers;
 using Dodo.RestaurantBoard.Tests.DSL;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
@@ -9,19 +14,22 @@ namespace Dodo.RestaurantBoard.Tests
     public class BoardsControllerTest
     {
         [Fact]
-        public void GetOrderReadinessToStationary_ShouldReturnOrderForPupaClientName()
+        public async Task GetOrderReadinessToStationary_ShouldReturnOrderForPupaClientName()
         {
             var departmentStructureService = Create.DepartmentsStructureService.WithPizzeria(Create.Pizzeria.Please()).Please();
-            var pizzeriaOrdersService = Create.PizzeriaOrdersService.Please();
+            var trackerClient = Create.TrackerClient.WithOrderFrom("Пупа").Please();
+            var pizzeriaOrdersService = Create.PizzeriaOrdersService.With(trackerClient).Please();
             var boardsController = new BoardsController(departmentStructureService, null, null, null, pizzeriaOrdersService);
 
-            var result = boardsController.GetOrderReadinessToStationary(unitId: 29).Result;
+            var result = await boardsController.GetOrderReadinessToStationary(unitId: 29);
 
+            Assert.Equal("Пупа", GetClientName(result));
+        }
 
-            JObject jobj = JObject.Parse(result.ToString());
-           // jobj["ClientOrders"][0]["ClientName"]
-
-            Assert.True(true);
+        private string GetClientName(JsonResult result)
+        {
+            var clientOrder = (result.Value.GetType().GetProperty("ClientOrders").GetValue(result.Value) as IEnumerable<object>).Single();
+            return clientOrder.GetType().GetProperty("ClientName").GetValue(clientOrder).ToString();
         }
     }
 }
