154db7059d20dc1d915d29b27cf4662bea722e96
antonyzudov <antony.zudov@mail.ru>
2018-08-22 10:06:34 +0300

    PizzeriaOrdersServiceTests

---
+++ Dodo.RestaurantBoard.Tests/PizzeriaOrdersServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Dodo.RestaurantBoard.Domain.Services;
+using Xunit;
+
+namespace Dodo.RestaurantBoard.Tests
+{
+    public class PizzeriaOrdersServiceTests
+    {
+        [Fact]
+        public void WhenGetOrdersByUnitIdAsync_ShouldReturnCorrectOrders()
+        {
+            var service = new PizzeriaOrdersService(
+                new DepartmentsStructureService(),
+                new TrackerClientStub());
+
+            var pizzeriaOrders = service.GetOrdersByUnitIdAsync(10).Result;
+
+            Assert.Equal(29, pizzeriaOrders.Pizzeria.Id);
+            Assert.Equal(2, pizzeriaOrders.Orders.Length);
+        }
+    }
+}
