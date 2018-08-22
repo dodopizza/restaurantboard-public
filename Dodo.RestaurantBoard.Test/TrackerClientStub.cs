8e28499f64314e22e34c25caa5d3e27307e67048
Nikita Starichenko <n.starichenko@dodopizza.com>
2018-08-22 10:11:09 +0300

    Add test for PizzeriaOrdersService

---
+++ Dodo.RestaurantBoard.Test/TrackerClientStub.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Dodo.Core.Common;
+using Dodo.Core.Services;
+using Dodo.Tracker.Contracts;
+using Dodo.Tracker.Contracts.Enums;
+
+namespace Dodo.RestaurantBoard.Test
+{
+    public class TrackerClientStub : ITrackerClient
+    {
+        public Task<ProductionOrder[]> GetOrdersByTypeAsync(Uuid unitUuid, OrderType type, int limit)
+        {
+            return Task.FromResult(new[]
+            {
+                new ProductionOrder(),
+                new ProductionOrder(),
+            });
+        }
+    }
+}
