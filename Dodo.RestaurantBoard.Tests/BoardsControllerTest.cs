fcb658882d5af4e7abb869cbc5b80f917645ffe1
AlexanderIB31 <aleks_bales@mail.ru>
2018-08-22 22:37:59 +0300

    Add test for BoardsController

---
+++ Dodo.RestaurantBoard.Tests/BoardsControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Dodo.RestaurantBoard.Site.Controllers;
+using Dodo.RestaurantBoard.Tests.DSL;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Dodo.RestaurantBoard.Tests
+{
+    public class BoardsControllerTest
+    {
+        [Fact]
+        public void GetOrderReadinessToStationary_ShouldReturnOrderForPupaClientName()
+        {
+            var pizzeriaOrdersService = Create.PizzeriaOrdersService.Please();
+            var boardsController = new BoardsController(null, null, null, null, pizzeriaOrdersService);
+
+            var result = boardsController.GetOrderReadinessToStationary(unitId: 29).Result;
+
+            Assert.True(true);
+        }
+    }
+}
