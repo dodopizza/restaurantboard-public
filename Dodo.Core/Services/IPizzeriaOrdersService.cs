c76a05d0254a6fcd46e34626b317a6178ae2c611
Andrew <Andrew25455@mail.ru>
2018-08-23 14:00:43 +0300

    Add class for include TrackerClient and DepartmentsStructureService

---
+++ Dodo.Core/Services/IPizzeriaOrdersService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dodo.Core.Services
+{
+  public  interface IPizzeriaOrdersService
+    {
+
+    }
+}
---
+++ src/Dodo.RestaurantBoard/Dodo.RestaurantBoard.Domain/Services/PizzeriaOrdersService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dodo.Core.DomainModel.OrderProcessing;
+using Dodo.Core.Services;
+using Dodo.Tracker.Contracts;
+using Dodo.Tracker.Contracts.Enums;
+
+namespace Dodo.RestaurantBoard.Domain.Services
+{
+    public class PizzeriaOrdersService : IPizzeriaOrdersService
+    {
+        private readonly IDepartmentsStructureService _departmentsStructureService;
+        private readonly ITrackerClient _trackerClient;
+
+        public PizzeriaOrdersService(IDepartmentsStructureService departmentsStructureService,
+                                     ITrackerClient trackerClient)
+        {           
+            _departmentsStructureService = departmentsStructureService;
+            _trackerClient = trackerClient;
+        }
+
+      
+    }
+
+}
