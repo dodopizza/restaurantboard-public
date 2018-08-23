3f8397ec9d7b3fb5620f3a6747ad53493d8c1a03
Timur Basyrov <timmthetiger@mail.ru>
2018-08-23 21:42:39 +0500

    DSL: ClientServiceBuilder

---
+++ Dodo.RestaurantBoard.Site.Tests/DSL/ClientsServiceBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dodo.Core.DomainModel.Clients;
+using Dodo.Core.Services;
+using Moq;
+
+namespace Dodo.RestaurantBoard.Site.Tests.DSL
+{
+    class ClientsServiceBuilder
+    {
+        private readonly Mock<IClientsService> _service;
+
+        public ClientsServiceBuilder WithoutIcons()
+        {
+            _service.Setup(x => x.GetIcons()).Returns(new ClientIcon[] { });
+            return this;
+        }
+
+        public IClientsService Please()
+        {
+            return _service.Object;
+        }
+    }
+}
