8a90faa4a3ffbe2e05fd9cd579d25a24bf20c0c6
Xneg <xnegxneg@gmail.com>
2018-08-28 20:45:23 +0300

    Добавил заготовку под тест.

---
+++ Dodo.RestaurantBoard.Site.Tests/TestFixture.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Net.Http;
+
+namespace Dodo.RestaurantBoard.Site.Tests
+{
+    public class TestFixture : IDisposable
+    {
+        private readonly TestServer _server;
+
+        public HttpClient Client { get; }
+
+        public TestFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<Core.Startup>();
+
+            _server = new TestServer(builder);
+
+            Client = _server.CreateClient();
+            Client.BaseAddress = new Uri("http://localhost:5000");
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            _server.Dispose();
+        }
+    }
+}
